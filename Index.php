4e12cb55dd4da82b3e3b2e04a8ec0836c81481c1
ToSzi <t.horvath@rgu.ac.uk>
2017-03-14 20:53:52 +0000

    Initial commit

---
+++ Index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html lang="eng">
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+<header>
+    <h1>This is h1</h1>
+</header>
+<main>
+    <form action="Index.php" method="post">
+        <fieldset>
+            <legend>Login</legend>
+            <label for="id">ID</label>
+            <input id="id" type="text" name="id" />
+            <label for="pw">Password</label>
+            <input id="pw" type="password" name="password" />
+        </fieldset>
+    </form>
+</main>
+</body>
+</html>
