a022404e545f867769f45342204ceec0721887dc
ToSzi <t.horvath@rgu.ac.uk>
2017-03-14 21:31:02 +0000

    Checklogin and home page added

---
+++ CheckLogin.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ToSzi
+ * Date: 3/14/2017
+ * Time: 9:13 PM
+ */
+    $username = $_POST['id'];
+    $password = $_POST['password'];
+
+    if(($username == "user") && ($password == "password"))
+    {
+        setcookie('accessLevelCookie', 'standardUser');
+    }
+
+    header('location: HomePage.php');
+?>
---
+++ HomePage.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ToSzi
+ * Date: 3/14/2017
+ * Time: 9:17 PM
+ */
+    echo"<p>HELLO</p>";
+?>
